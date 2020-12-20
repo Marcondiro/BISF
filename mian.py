68430099dc322d29a1cdc93337ea3d4edf07ef7d
Marcondiro <46560192+Marcondiro@users.noreply.github.com>
2020-12-20 12:04:48 +0100

    Added packages, updated requirements

---
+++ mian.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-s
+
+import numpy as np
+import pandas as pd
+pd.core.common.is_list_like = pd.api.types.is_list_like
+import pandas_datareader.data as web
+import matplotlib.echopyplot as plt
